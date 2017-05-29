b65bf275d50b32c4f713453aef977f35afe13aa5
kifran72 <cr91700kifran@gmail.com>
2017-05-29 13:45:27 +0200

    ok!

---
+++ controler/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Config {
+    const SERVERNAME="localhost";
+    const DBNAME="conciergin";
+    const USER="root";
+    const PASSWORD="toor";
+}
+
+
+?>	
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include "vue/Accueil.php";
+
+?>
